b82d4573f77fb6bf704db01274f145a1b102fcf0
Eeshan Kumar <me@eeshankumar.com>
2015-04-28 00:48:16 -0700

    Add cmd line processing and test

--- test/greet_test.js
+++ test/greet_test.js
@@ -2,11 +2,26 @@
 
 var expect = require('chai').expect;
 var greet = require('../greet');
+var exec = require('child_process').exec;
 
 describe('greet.js', function() {
   it('should greet someone', function() {
     expect(greet('Kumar')).to.eql('hello Kumar');
   });
 
-  it('has a pending test');
+  var cmdLineOutput;
+
+  before(function(done) {
+    exec('node greet.js somename', function (error, stdout, stderr) {
+        cmdLineOutput = stdout;
+        done();
+    });
+  });
+
+  it('should process command line arguments', function() {
+    expect(cmdLineOutput).to.eql('hello somename\n');
+  });
 });
+
+
+
